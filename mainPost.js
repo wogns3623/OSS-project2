d586bf2674f5263877f3aafd6261208bf5d1e60c
김재훈 <wogns3623@gmail.com>
2018-12-12 08:34:32 +0900

    공유시 공유한 포스트만 표시되는 페이지로 연결되는 기능 추가

--- mainPost.js
+++ mainPost.js
@@ -1,44 +1,84 @@
+function getQueryStringObject() {
+    var a = window.location.search.substr(1).split('&');
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+        var p = a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+}
+
 var request = new XMLHttpRequest();
 var serverAddress = "https://opso.ml/board";
-// var serverAddress = "http://18.216.159.54/board";
+var data;
+var getPostTF = false;
+var getPostByIDTF = false;
+// var serverAddress = "http://18.216.159.54/board"; +"?offset="+i
 
 function getPost() {
     request.open("GET", serverAddress, true);
+    getPostTF = true;
     request.send();
 }
 
-getPost();
+function getPostByID(postID) {
+    getPostByIDTF = true;
+    request.open("GET", serverAddress + "/" + postID, true);
+    request.send();
 
-// request.onload = function() {
-//   if (request.status === 200 || request.status === 201) {
-//     console.log(request.responseText);
-//   } else {
-//     console.error(request.responseText);
-//   }
-// };
+    console.log(data);
+}
 
 request.onreadystatechange = function (event) {
     if (request.readyState == 4) {
         if (request.status == 200) {
-            var data = JSON.parse(JSON.parse(request.responseText));
-            var articles = "";
-            for (i = 0; i < data.length; i++) {
-                content = data[i];
-                author = content["author"];
-                contents = content["contents"];
-                article = '<div class="view_box"><div class="userInfo"><div class="user_icon">';
-                article += author[0];
-                article += '</div><div class="user_name">';
-                article += author;
-                article += '</div></div><div class="view_article">';
-                article += contents;
-                article += '</div><div class="view_buttonBox"><i class="fab fa-facebook-square" onclick="popUpFacebook()"></i><div class="margin" id="buttonBox_middle"></div><i class="fab fa-twitter-square" onclick="popUpTwitter()"></i><div class="margin" id="buttonBox_middle"></div><i class="fab fa-line" onclick="popUpLine()"></i></div></div>';
-
-                articles += article;
+            data = JSON.parse(JSON.parse(request.responseText));
+            if (getPostTF) {
+                getPostTF = false;
+                var articles = dataToArticle(data);
+                document.getElementById("view_wrap").innerHTML = articles;
+            } else if (getPostByIDTF) {
+                getPostByIDTF = false;
+                var articles = dataToArticle(data);
+                document.getElementById("contents_wrap").innerHTML = articles;
             }
-            document.getElementById("view_wrap").innerHTML = articles;
         }
     }
+}
+
+function dataToArticle(data) {
+    var articles = "";
+    if (data.length == undefined) {
+        var article = '<div class="view_box"><div class="userInfo"><div class="user_icon">';
+        article += data.author[0];
+        article += '</div><div class="user_name">';
+        article += data.author;
+        article += '</div><div class="post_ID">' + data.id + '</div></div><div class="view_article">';
+        article += data.contents;
+        article += '</div><div class="view_buttonBox"><i class="fab fa-facebook-square" onclick="popUpFacebook()"></i><div class="margin" id="buttonBox_middle"></div><i class="fab fa-twitter-square" onclick="popUpTwitter()"></i><div class="margin" id="buttonBox_middle"></div><i class="fab fa-line" onclick="popUpLine()"></i></div></div>';
+
+        articles += article;
+        return articles;
+    }
+    for (i = 0; i < data.length; i++) {
+        var content = data[i];
+        var author = content["author"];
+        var contents = content["contents"];
+        var article = '<div class="view_box"><div class="userInfo"><div class="user_icon">';
+        article += author[0];
+        article += '</div><div class="user_name">';
+        article += author;
+        article += '</div><div class="post_ID">' + content["id"] + '</div></div><div class="view_article">';
+        article += contents;
+        article += '</div><div class="view_buttonBox"><i class="fab fa-facebook-square" onclick="popUpFacebook(' + content["id"] + ')"></i><div class="margin" id="buttonBox_middle"></div><i class="fab fa-twitter-square" onclick="popUpTwitter(' + content["id"] + ')"></i><div class="margin" id="buttonBox_middle"></div><i class="fab fa-line" onclick="popUpLine(' + content["id"] + ')"></i></div></div>';
+
+        articles += article;
+    }
+    return articles;
 }
 
 function clickSendBtn() {
@@ -63,22 +103,35 @@ function clickSendBtn() {
 
 }
 
-function popUpTwitter() {
+function popUpTwitter(postID) {
     // window.open("https://naver.com/", "", "left=400, top=400, width=400, height=400, scrollbars=0, toolbar=0, menubar=0");
     window.open("https://twitter.com/intent/tweet?" +
         "&text=" + encodeURIComponent("WhatTimeIsItNow?") +
-        "&url=" + encodeURIComponent(window.location.href), "share!", "left=400, top=200, width=600, height=400, scrollbars= 0, toolbar=0, menubar=0"
+        "&url=" + encodeURIComponent(window.location.href+"?postID="+postID), "share!", "left=400, top=200, width=600, height=400, scrollbars= 0, toolbar=0, menubar=0"
     );
 };
 
 function popUpFacebook() {
     window.open("https://www.facebook.com/sharer/sharer.php" +
-        "?u=" + encodeURIComponent(window.location.href), "share!", "left=400, top=200, width=600, height=400, scrollbars= 0, toolbar=0, menubar=0"
+        "?u=" + encodeURIComponent(window.location.href+"?postID="+postID), "share!", "left=400, top=200, width=600, height=400, scrollbars= 0, toolbar=0, menubar=0"
     );
 };
 
 function popUpLine() {
-    window.open("https://social-plugins.line.me/lineit/share"
-    +"?url=" + encodeURIComponent(window.location.href), "share!", "left=400, top=200, width=600, height=400, scrollbars= 0, toolbar=0, menubar=0"
+    window.open("https://social-plugins.line.me/lineit/share" +
+        "?url=" + encodeURIComponent(window.location.href+"?postID="+postID), "share!", "left=400, top=200, width=600, height=400, scrollbars= 0, toolbar=0, menubar=0"
     );
 };
+
+var qs = getQueryStringObject();
+console.log(qs.postID);
+
+if (qs.postId != undefined) {
+    document.getElementById("contents_wrap").innerHTML = "";
+    getPostByID(qs.postID);
+} else {
+    document.getElementById("contents_wrap").innerHTML = '<section class="post" id="post_wrap"><form action="https://opso.ml/board" method="POST"><textarea name="author" id="post_name" placeholder="이름"></textarea><textarea name="contents" id="post_article" placeholder="내용" rows="3"></textarea><button id="post_send" type="reset" onclick="clickSendBtn()">send</button></form></section><section class="view" id="view_wrap"></section>';
+    getPost();
+}
+
+//'<section class="post" id="post_wrap"><form action="https://opso.ml/board" method="POST"><textarea name="author" id="post_name" placeholder="이름"></textarea><textarea name="contents" id="post_article" placeholder="내용" rows="3"></textarea><button id="post_send" type="reset" onclick="clickSendBtn()">send</button></form></section><section class="view" id="view_wrap"></section>';
